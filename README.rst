ff2a6070811bd986c55f2d7db2dc5a5855280d49
lshuns <1006682997@qq.com>
2020-12-13 17:27:11 +0100

    v0.1

--- README.rst
+++ README.rst
@@ -2,53 +2,76 @@
  SPP_HMDB: Search PiPeline for the Human Metabolome Database (HMDB)
 ===================================================================
 
-Simple pipeline for quest of information in the Human Metabolome Database (HMDB, https://hmdb.ca/).
+Simple pipeline for quest of information in the Human Metabolome Database (HMDB, https://hmdb.ca/). 
+
+Build the local database
+-----------------------
+
+1. Download the latest database from https://hmdb.ca/system/downloads/current/hmdb_metabolites.zip
+
+2. run xml2feather.py from scripts/ (Warning: demanding mechine memory required, better to run on a cluster.)
+
+
+Query
+-----------------------
+
+1. Go to running/ and build `desired_info.param' and  `input_info.param' as the form shown in corresponding example files.
+
+2. Run run.py
+
+3. Find desired table file in results/
+
 
 Current supported tags:
 -----------------------
 
-abnormal_concentrations
-HMDB_ID
-average_molecular_weight
-bigg_id
-biological_properties
-cas_registry_number
-chebi_id
-chemical_formula
-chemspider_id
+version
 creation_date
-cs_description
+update_date
+HMDB_ID
+status
+name
 description
-diseases
-drugbank_id
-experimental_properties
-foodb_id
-general_references
+chemical_formula
+average_molecular_weight
+monisotopic_molecular_weight
+iupac_name
+traditional_iupac
+cas_registry_number
+smiles
 inchi
 inchikey
-iupac_name
-kegg_id
-knapsack_id
-meta_cyc_id
-metlin_id
-monisotopic_molecular_weight
-name
-normal_concentrations
-ontology
-pdb_id
-phenol_explorer_compound_id
-predicted_properties
-protein_associations
-pubchem_compound_id
-secondary_accessions
-smiles
-spectra
 state
-status
-synonyms
-synthesis_reference
-taxonomy
-traditional_iupac
-update_date
-version
+kegg_id
+foodb_id
+chemspider_id
+drugbank_id
+pdb_id
+chebi_id
+pubchem_compound_id
+biocyc_id
 wikipedia_id
+knapsack_id
+phenol_explorer_compound_id
+bigg_id
+metlin_id
+vmh_id
+fbonto_id
+synthesis_reference
+synonyms
+
+TBD tags:
+-----------------------
+
+secondary_accessions
+taxonomy
+ontology
+experimental_properties
+predicted_properties
+spectra
+biological_properties
+normal_concentrations
+abnormal_concentrations
+diseases
+general_references
+protein_associations
--- scripts/xml2feather.py
+++ scripts/xml2feather.py
@@ -9,6 +9,7 @@ Scripts to transfer xml-form info to feather-form info
 """
 
 import xml.etree.ElementTree as et
+from lxml import etree
 
 import numpy as np
 import pandas as pd
@@ -27,9 +28,11 @@ def parse_XML_main(xml_file):
     # Use the first element's tags 
     #   as the reference
     cols = []
+    cols_name = []
     for child in root[0]:
         cols.append(child.tag)
-    print(cols)
+        cols_name.append(child.tag[20:])
+    # print(cols)
     
     rows = []
     for node in root:
@@ -43,7 +46,37 @@ def parse_XML_main(xml_file):
                 vals.append(val)
             else:
                 vals.append(None)
-        rows.append({cols[i]: vals[i]
+        rows.append({cols_name[i]: vals[i]
+                        for i in range(len(cols))})
+    
+    df = pd.DataFrame(data=rows)
+    df.rename(columns={'accession': 'HMDB_ID'}, inplace=True)
+
+    return df      
+
+def parse_XML_synonyms(xml_file): 
+    """
+    Extract synonyms for all the elements.
+        for searching using synonyms.
+    """
+
+    rows = []
+    for _, element in etree.iterparse(xml_file, tag='{http://www.hmdb.ca}synonyms'):
+        if element.getparent().tag == '{http://www.hmdb.ca}metabolite':
+            metabolite = element.getparent()
+
+            # HMDB_ID
+            HMDB_ID = metabolite.findtext('{http://www.hmdb.ca}accession')
+            cols = ['HMDB_ID']
+            vals = [HMDB_ID]
+
+            # synonyms
+            synonyms_node = metabolite.find('{http://www.hmdb.ca}synonyms')
+            for i_syn in range(len(synonyms_node)):
+                cols.append(f'synonym_{i_syn+1}')
+                vals.append(synonyms_node[i_syn].text.lower())
+
+            rows.append({cols[i]: vals[i]
                         for i in range(len(cols))})
     
     df = pd.DataFrame(data=rows)
@@ -53,16 +86,32 @@ def parse_XML_main(xml_file):
 if __name__ == "__main__":
     import time
   
-    # ++++++++++++++++++++++++++ Setting
+    # # ++++++++++++++++++++++++++ main data
+    # # original data
+    # inpath = "../data/hmdb_metabolites.xml"
+    # # out data
+    # outpath = "../data/hmdb_metabolites_main.feather"
+    # # Running
+    # Start = time.time()
+    # # main tags' info
+    # # parse_XML_main(inpath)
+    # df_main = parse_XML_main(inpath)
+    # df_main.to_feather(outpath)
+    # print("main tags' info saved in", outpath)
+    # print("Finished in", time.time()-Start)
+    # # Finished in 308.7
+
+    # ++++++++++++++++++++++++++ synonyms info
     # original data
     inpath = "../data/hmdb_metabolites.xml"
     # out data
-    outpath = "../data/hmdb_metabolites_main.feather"
-
-    # ++++++++++++++++++++++++++ Running
+    outpath = "../data/hmdb_metabolites_synonyms.feather"
+    # Running
     Start = time.time()
-    # main tags' info
-    df_main = parse_XML_main(inpath)
-    df_main.to_feather(outpath)
-    print("main tags' info saved in", outpath)
+    # tags' info
+    df_syn = parse_XML_synonyms(inpath)
+    print(df_syn)
+    df_syn.to_feather(outpath)
+    print("Synonyms info saved in", outpath)
     print("Finished in", time.time()-Start)
+    # Finished in 208.3602416515350
